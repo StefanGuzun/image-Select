e925e95bc163bca9eb76182a9dee6aa6756555ba
DoomPin <pynzaru.dima@mail.ru>
2022-07-12 14:52:13 +0300

    MainPage Structure ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css';
+import MainPage from './components/MainPage/MainPage';
+import "./components/MainPage/MainPage.css";
 
 function App() {
   return (
     <div className="App">
-
+      <MainPage/>
     </div>
   );
 }
