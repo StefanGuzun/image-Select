20924a0706004e73d01f290dcaf0b164eb306c93
Adelina <tapesadelina@gmail.com>
2022-07-13 10:11:15 +0300

    dialog-pop-up-connection

--- src/components/Dialog/Dialog.tsx
+++ src/components/Dialog/Dialog.tsx
@@ -1,29 +1,31 @@
+import { MouseEventHandler, ReactElement, JSXElementConstructor, ReactFragment, ReactPortal } from "react";
 import "./Dialog.css"
 
-const RealNotReal = () => {
-  const features = ["Background", "Eyes", "Nose", "Hair", "Mouth", "Ears", "Jewellery"]
-  return (
-      <div style={{margin:"0 330px", border:"black solid "}}>
-        <h3 style={{display:"flex", justifyContent:"center", marginBottom:"68px"}}>Why</h3>
-        <label>Choose features
-          <ul className="featuresContainer">
-            {
-              features.map((feature) => <li style={{listStyleType:"none"}}><label style={{display:"flex", alignItems:"center"}}><input type="checkbox" style={{height:"25px", width:"25px"}}/>{feature}</label></li>)
-            }
-          </ul>
-        </label>
-        <p style={{margin:"0 20px"}}>Other</p>
-        <div style={{display:"flex",  justifyContent:"center"}}><input type="text" className="input" placeholder="Enter your own feature"/>
+const Dialog = (props: { handleClose: MouseEventHandler<HTMLSpanElement> | undefined; }) => {
+    const features = ["Background", "Eyes", "Nose", "Hair", "Mouth", "Ears", "Jewellery"]
+    return (
+        <div className="wholeContent popup-box" style={{ border:"black solid "}}>
+            <span className="close-icon" onClick={props.handleClose}>x</span>
+            <h3 style={{display:"flex", justifyContent:"center", marginBottom:"68px"}}>Why</h3>
+            <label>Choose features
+                <ul className="featuresContainer">
+                    {
+                        features.map((feature) => <li style={{listStyleType:"none"}}><label style={{display:"flex", alignItems:"center"}}><input type="checkbox" style={{height:"25px", width:"25px"}}/>{feature}</label></li>)
+                    }
+                </ul>
+            </label>
+            <p style={{margin:"0 20px"}}>Other</p>
+            <div style={{display:"flex",  justifyContent:"center"}}><input type="text" className="inputStyle" placeholder="Enter your own feature"/>
+            </div>
+            <div style={{marginTop:"120px", display:"flex", justifyContent:"space-evenly"}}>
+                <button className="buttonStyle"  onClick={props.handleClose}>
+                    Cancel
+                </button>
+                <button className="buttonStyle">
+                    Ok
+                </button>
+            </div>
         </div>
-        <div style={{marginTop:"190px", display:"flex", justifyContent:"space-evenly"}}>
-          <button className="buttonStyle">
-            Cancel
-          </button>
-          <button className="buttonStyle">
-            Ok
-          </button>
-        </div>
-      </div>
-  )
+    )
 }
-export default RealNotReal
+export default Dialog
