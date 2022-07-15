f327ff570b00b2015143901c6ccbda5e4b504c8d
sjnt <bragav731@gmail.com>
2022-07-15 12:46:46 +0300

    tried to implement redux

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -4,6 +4,8 @@ import "./MainPage.css"
 import Dialog from "../Dialog/Dialog";
 import APIComponent from "../../API";
 import { TransformComponent, TransformWrapper } from "react-zoom-pan-pinch";
+import { useSelector, useDispatch } from "react-redux";
+import { real, notReal } from '../../redux/store'
 
 const MainPage = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -11,9 +13,9 @@ const MainPage = () => {
     setIsOpen(!isOpen);
   }
 
-  const realImg = () => {
-      window.location.reload()
-  }
+  const buttons = useSelector(state => state);
+  const dispatch = useDispatch<any>();
+
     return (
       <div className="MainContainer">{isOpen && <Dialog handleClose={togglePopup}/>}
       <div className="UpperContainer">
@@ -27,8 +29,8 @@ const MainPage = () => {
           </TransformWrapper>
         </div>
         <div className="ButtonPosition">
-            <input type="button" className="ButtonStyle" value="Real" onClick={realImg}/>
-            <input type="button" className="ButtonStyle" value="Not Real" onClick={togglePopup}/>
+            <input type="button" className="ButtonStyle" value="Real" onClick={dispatch(real)}/>
+            <input type="button" className="ButtonStyle" value="Not Real" onClick={() => dispatch(notReal)}/>
         </div>
       </div>
       <div style={{height: "50%", width: "100%"}}>
