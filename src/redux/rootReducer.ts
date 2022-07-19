46c835e6ec66bc308df49a43da9110f7295a3018
stefan <stefanguzun2005@gmail.com>
2022-07-19 13:50:49 +0300

    i removed the redux

--- src/redux/rootReducer.ts
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux"
-import { imgReducer } from "./imgReducer"
-
-export const rootReducer = combineReducers({
-    image: imgReducer
-})
-
-export type RootState = ReturnType<typeof rootReducer>
